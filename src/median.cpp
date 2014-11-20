bc312e263da6259cd5441e2f9b943338ce23fcac
Prasant Chidella <prasant12075@iiitd.ac.in>
2014-11-20 17:23:53 +0530

    mode done : output has a lot of noise

--- src/median.cpp
+++ src/median.cpp
@@ -8,6 +8,8 @@ using namespace std;
 using namespace cv;
 
 void median(Mat* src_imgs, Mat& dst);
+int find_median_of_vector(vector<int> vals);
+int find_mode_of_vector(vector<int> vals);
 
 int main(int argc, char const *argv[])
 {
@@ -26,55 +28,44 @@ int main(int argc, char const *argv[])
     src_imgs[11] = imread("../input/vlcsnap-2014-11-20-14h22m57s28.tiff", CV_LOAD_IMAGE_COLOR);
     src_imgs[12] = imread("../input/vlcsnap-2014-11-20-14h22m57s30.tiff", CV_LOAD_IMAGE_COLOR);
 
-    // imshow("first", src_imgs[8]);
-
     Mat median_img = Mat(src_imgs[0].size(), src_imgs[0].type());
     median(src_imgs, median_img);
 
     if (!src_imgs[0].data) {
         cout << src_imgs[0].size() << "no data in median_img" << endl;
     } else {
-        imshow("median", median_img);
+        // imshow("median", median_img);
+        imwrite("../output/output_median.tiff", median_img);
     }
 
-    waitKey(0);
+    // waitKey(0);
     return 0;
 }
 
 void median(Mat* src_imgs, Mat& dst) {
     Mat color_channels[input_img_count][3];
 
-    for (int n = 0; n < input_img_count; ++n)
-    {
+    for (int n = 0; n < input_img_count; ++n) {
         split(src_imgs[n], color_channels[n]);
     }
-    imshow("0", color_channels[0][0]);
-    imshow("1", color_channels[0][0]);
-    imshow("2", color_channels[0][0]);
 
     Mat channel_b = Mat(color_channels[0][0].size(), color_channels[0][0].type());
     Mat channel_g = Mat(color_channels[0][1].size(), color_channels[0][1].type());
     Mat channel_r = Mat(color_channels[0][2].size(), color_channels[0][2].type());
 
-    for (int i = 0; i < src_imgs[0].rows; ++i)
-    {
-        for (int j = 0; j < src_imgs[0].cols; ++j)
-        {
+    for (int i = 0; i < src_imgs[0].rows; ++i) {
+        for (int j = 0; j < src_imgs[0].cols; ++j) {
             vector<int> pixels[3];
-            for (int k = 0; k < 3; ++k)
-            {
-                for (int l = 0; l < input_img_count; ++l)
-                {
+            for (int k = 0; k < 3; ++k) {
+                for (int l = 0; l < input_img_count; ++l) {
                     pixels[k].push_back(color_channels[l][k].at<uchar>(i, j));
                 }
             }
 
-            nth_element(pixels[0].begin(), pixels[0].begin()+pixels[0].size()/2, pixels[0].end());
-            channel_b.at<uchar>(i, j) = pixels[0][input_img_count/2];
-            nth_element(pixels[1].begin(), pixels[1].begin()+pixels[1].size()/2, pixels[1].end());
-            channel_g.at<uchar>(i, j) = pixels[1][input_img_count/2];
-            nth_element(pixels[2].begin(), pixels[2].begin()+pixels[2].size()/2, pixels[2].end());
-            channel_r.at<uchar>(i, j) = pixels[2][input_img_count/2];
+            channel_b.at<uchar>(i, j) = find_median_of_vector(pixels[0]);
+            channel_g.at<uchar>(i, j) = find_median_of_vector(pixels[1]);
+            channel_r.at<uchar>(i, j) = find_median_of_vector(pixels[2]);
+
         }   
     }
 
@@ -82,7 +73,18 @@ void median(Mat* src_imgs, Mat& dst) {
     merge(to_merge, 3, dst);
 }
 
+int find_median_of_vector(vector<int> vals) {
+    nth_element(vals.begin(), vals.begin()+vals.size()/2, vals.end());
+    return vals[vals.size()/2];
+}
 
+int find_mode_of_vector(vector<int> vals) {
+    vector<int> freq_table(256,0);
+    for( int i=0; i<vals.size(); ++i ) {
+      freq_table[vals[i]]++;
+    }
+    return max_element( freq_table.begin(), freq_table.end() ) - freq_table.begin();
+}
 
 
 
