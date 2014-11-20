c528bb379fa4bbae0901e9b72692279c91c85d8a
Prasant Chidella <prasant12075@iiitd.ac.in>
2014-11-20 16:54:33 +0530

    median done

--- src/median.cpp
+++ src/median.cpp
@@ -12,19 +12,19 @@ void median(Mat* src_imgs, Mat& dst);
 int main(int argc, char const *argv[])
 {
     Mat src_imgs[input_img_count];
-    src_imgs[0] = imread("../input/vlcsnap-2014-11-20-14h22m55s6.tiff", CV_8UC3);
-    src_imgs[1] = imread("../input/vlcsnap-2014-11-20-14h22m55s8.tiff", CV_8UC3);
-    src_imgs[2] = imread("../input/vlcsnap-2014-11-20-14h22m55s10.tiff", CV_8UC3);
-    src_imgs[3] = imread("../input/vlcsnap-2014-11-20-14h22m55s12.tiff", CV_8UC3);
-    src_imgs[4] = imread("../input/vlcsnap-2014-11-20-14h22m55s14.tiff", CV_8UC3);
-    src_imgs[5] = imread("../input/vlcsnap-2014-11-20-14h22m56s16.tiff", CV_8UC3);
-    src_imgs[6] = imread("../input/vlcsnap-2014-11-20-14h22m56s18.tiff", CV_8UC3);
-    src_imgs[7] = imread("../input/vlcsnap-2014-11-20-14h22m56s20.tiff", CV_8UC3);
-    src_imgs[8] = imread("../input/vlcsnap-2014-11-20-14h22m56s22.tiff", CV_8UC3);
-    src_imgs[9] = imread("../input/vlcsnap-2014-11-20-14h22m56s24.tiff", CV_8UC3);
-    src_imgs[10] = imread("../input/vlcsnap-2014-11-20-14h22m57s26.tiff", CV_8UC3);
-    src_imgs[11] = imread("../input/vlcsnap-2014-11-20-14h22m57s28.tiff", CV_8UC3);
-    src_imgs[12] = imread("../input/vlcsnap-2014-11-20-14h22m57s30.tiff", CV_8UC3);
+    src_imgs[0] = imread("../input/vlcsnap-2014-11-20-14h22m55s6.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[1] = imread("../input/vlcsnap-2014-11-20-14h22m55s8.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[2] = imread("../input/vlcsnap-2014-11-20-14h22m55s10.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[3] = imread("../input/vlcsnap-2014-11-20-14h22m55s12.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[4] = imread("../input/vlcsnap-2014-11-20-14h22m55s14.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[5] = imread("../input/vlcsnap-2014-11-20-14h22m56s16.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[6] = imread("../input/vlcsnap-2014-11-20-14h22m56s18.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[7] = imread("../input/vlcsnap-2014-11-20-14h22m56s20.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[8] = imread("../input/vlcsnap-2014-11-20-14h22m56s22.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[9] = imread("../input/vlcsnap-2014-11-20-14h22m56s24.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[10] = imread("../input/vlcsnap-2014-11-20-14h22m57s26.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[11] = imread("../input/vlcsnap-2014-11-20-14h22m57s28.tiff", CV_LOAD_IMAGE_COLOR);
+    src_imgs[12] = imread("../input/vlcsnap-2014-11-20-14h22m57s30.tiff", CV_LOAD_IMAGE_COLOR);
 
     // imshow("first", src_imgs[8]);
 
@@ -48,6 +48,9 @@ void median(Mat* src_imgs, Mat& dst) {
     {
         split(src_imgs[n], color_channels[n]);
     }
+    imshow("0", color_channels[0][0]);
+    imshow("1", color_channels[0][0]);
+    imshow("2", color_channels[0][0]);
 
     Mat channel_b = Mat(color_channels[0][0].size(), color_channels[0][0].type());
     Mat channel_g = Mat(color_channels[0][1].size(), color_channels[0][1].type());
@@ -58,11 +61,11 @@ void median(Mat* src_imgs, Mat& dst) {
         for (int j = 0; j < src_imgs[0].cols; ++j)
         {
             vector<int> pixels[3];
-            for (int k = 0; k < input_img_count; ++k)
+            for (int k = 0; k < 3; ++k)
             {
-                for (int l = 0; l < 3; ++l)
+                for (int l = 0; l < input_img_count; ++l)
                 {
-                    pixels[l].push_back(color_channels[k][l].at<uchar>(i, j));
+                    pixels[k].push_back(color_channels[l][k].at<uchar>(i, j));
                 }
             }
 
